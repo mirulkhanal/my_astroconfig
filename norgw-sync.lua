e40fa9c44593e1d0720cee39b8e8438cf599cb69
mirulkhanal <mirulkhanal@gmail.com>
2023-10-02 20:23:06 +0545

    updated checks for local repo being behind remote repo

--- norgw-sync.lua
+++ norgw-sync.lua
@@ -1,4 +1,3 @@
--- norgw-sync.lua
 local M = {}
 
 -- Specify the local path for the Git repository
@@ -10,26 +9,42 @@ local function get_commit_message()
     return string.format("dev@%s_%s", user_name, pc_name)
 end
 
-M.sync_workspace = function()
+function M.sync_workspace()
     -- Change into the Git repository directory
     vim.cmd("cd " .. git_repo_path)
 
     -- Example: print a message
     print("Syncing Neorg workspace in directory: " .. git_repo_path)
 
-    -- Example: Check if there are changes in the working directory
-    local status = vim.fn.system({'git', 'status', '--porcelain'})
-    if status ~= "" then
-        -- There are changes in the working directory
-        print("Changes detected. Committing and pushing...")
-        vim.fn.system({'git', 'add', '.'})
-        local commit_message = get_commit_message()
-        vim.fn.system({'git', 'commit', '-m', commit_message})
-        vim.fn.system({'git', 'push'})
-        print('Sync Success. 😀')
+    -- Run Git fetch to update remote branches
+    vim.fn.system("git fetch")
+
+    -- Check if the local branch is behind the remote branch
+    local is_behind = vim.fn.system("git status -uno | grep 'Your branch is behind'") ~= ""
+
+    if is_behind then
+        -- Local is behind, pull changes
+        print("Local branch is behind. Pulling changes...")
+        vim.fn.system("git pull")
+        print('Pull Success. 😀')
     else
-        -- Nothing to sync
-        print("Workspace is up to date.")
+        -- Nothing to pull, add, commit, and push changes
+        print("Local repository is up to date. Adding, committing, and pushing changes...")
+
+        -- Example: Check if there are changes in the working directory
+        local status = vim.fn.system({'git', 'status', '--porcelain'})
+        if status ~= "" then
+            -- There are changes in the working directory
+            print("Changes detected. Committing and pushing...")
+            vim.fn.system({'git', 'add', '.'})
+            local commit_message = get_commit_message()
+            vim.fn.system({'git', 'commit', '-m', commit_message})
+            vim.fn.system({'git', 'push'})
+            print('Push Success. 😀')
+        else
+            -- Nothing to sync
+            print("No local changes to sync.")
+        end
     end
 end
 
